cfeb474ef46dbc67ac993fd377fd4886201cc17c
abdulkadiriba <abdulkadiriba@gmail.com>
2023-09-15 19:55:16 +0300

    a

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,31 @@
 <template>
-  <div class="fixed top-16 w-72">asd
+  <div class="fixed top-16 w-72">
+    <div class="animate-text text-4xl text-white">FRONTENDCHARM</div>
   </div>
 </template>
 
 <script setup>
 
 </script>
+<style>
+
+.animate-text {
+  letter-spacing: 1vw;
+  background: url("https://thumbs.dreamstime.com/b/tropical-flower-pattern-seamless-flowers-blossom-flowers-nature-background-vector-illustration-61142748.jpg");
+  background-size: auto 200%;
+  background-clip: text;
+  text-fill-color: transparent;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  animation: shine 8s linear infinite;
+}
+
+@keyframes shine {
+  from {
+    background-position: center 0;
+  }
+  to {
+    background-position: center 200%;
+  }
+}
+</style>
--- pages/technologies.vue
+++ pages/technologies.vue
@@ -17,21 +17,21 @@ const technologies = ref([
     <div class="text-4xl font-semibold">Technologies I Use</div>
     <div class="grid lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5 mt-12">
       <div v-for="tech in technologies" :key="tech.name" class="technologies group relative">
-        <div class="w-full bg-zinc-500/30 absolute group-hover:h-32 h-0 duration-500 bottom-0 transition-all"></div>
-        <div class="px-5 py-3.5">
-          <img :src="tech.img" class="h-12 " :alt="tech.name" />
-        <div class="text-xl group-hover: duration-200">{{ tech.name }}</div>
+        <div class="w-full bg-zinc-800 absolute group-hover:h-32 h-0 duration-700 bottom-0 transition-all"></div>
+        <div class="px-5 py-3.5 flex justify-between items-center">
+          <img :src="tech.img" class="h-12 group-hover:translate-x-3 duration-500" :alt="tech.name" />
+          <div class="text-xl group-hover:-translate-x-9 duration-500">{{ tech.name }}</div>
         </div>
-
       </div>
     </div>
   </div>
 </template>
 <style>
-.technologies{
-  @apply duration-200 flex bg-zinc-500/40 justify-between items-center rounded-lg border border-zinc-600/80 overflow-hidden
+.technologies {
+  @apply duration-200 bg-zinc-500/40 rounded-lg border border-zinc-600/80 overflow-hidden transition-all;
 }
-.technologies:hover{
-  @apply scale-105 bg-zinc-700 border-zinc-400
+
+.technologies:hover {
+  @apply scale-105 bg-zinc-700 border-zinc-400;
 }
 </style>
